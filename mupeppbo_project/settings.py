d8bf689a7795bd70eaaf51e3a8fbf45073760502
goodcodelove <coulibalyissa956@gmail.com>
2022-12-24 15:52:01 +0000

    fix static_root

--- mupeppbo_project/settings.py
+++ mupeppbo_project/settings.py
@@ -125,10 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR/'staticfiles',
-]
+STATIC_ROOT = 'mupeppbo/static/'
 
 MEDIA_URL = 'medias/'
 MEDIA_ROOT = BASE_DIR / 'medias'
