6f9fee467abc0c0857c8a5db54babfba1872680f
goodcodelove <coulibalyissa956@gmail.com>
2022-12-27 16:02:11 +0000

    fix media_url

--- mupeppbo_project/settings.py
+++ mupeppbo_project/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 
-MEDIA_URL = 'medias/'
+MEDIA_URL = '/medias/'
 MEDIA_ROOT = BASE_DIR / 'medias/'
 
 # Default primary key field type
