3aae75b6e5bf00462f36be88e2301591a4917aa4
goodcodelove <coulibalyissa956@gmail.com>
2023-01-12 03:31:48 +0000

    change en->fr

--- mupeppbo_project/settings.py
+++ mupeppbo_project/settings.py
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-ENG'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
