253d7d0e5a9499df128be3d907f8d5cbcaf960da
goodcodelove <coulibalyissa956@gmail.com>
2023-01-19 17:34:43 +0000

    hot fix

--- message/admin.py
+++ message/admin.py
@@ -22,56 +22,32 @@ class SmsAdmin(admin.ModelAdmin):
 
     @admin.action(description="Envoyer sms")
     def send_sms(self, request, queryset):
-        # token = generate_token()
-        # m = []
-        # for q in queryset:
-        #     m += q.mutualist.all()
-        # m_list = [m.phone_number for m in m]
-        # body = ' '.join([qs.body for qs in queryset])
-
-        # url = "https://api.orange.com/smsmessaging/v1/outbound/tel%3A%2B2250504522224/requests"
-
-        # headers = {
-        #     "Authorization": f"Bearer {token}",
-        #     "Content-Type": "application/json"
-        # }
-
-        # for nb in m_list:
-        #     payload = { 
-        #     "outboundSMSMessageRequest": {
-		#             "address": f"tel:+225{nb}",
-		#             "senderAddress":"tel:+2250504522224",
-        #             "senderName": "MUPEPPBO",
-		#             "outboundSMSTextMessage": {
-        #                 "message": f"{body}"
-        #             }
-	    #         }
-        #     }
-        #     response = requests.post(url, json=payload, headers=headers)
-
-        #     json_response = response.json()
-        #     print(json_response)
         token = generate_token()
-        for qs in queryset:
-            body = qs.body
-            phone_number = [m.phone_number for m in qs.mutualist.all()]
-        
-            for p in phone_number:
-                url = "https://api.orange.com/smsmessaging/v1/outbound/tel%3A%2B2250504522224/requests"
+        m = []
+        for q in queryset:
+            m += q.mutualist.all()
+        m_list = [m.phone_number for m in m]
+        body = ' '.join([qs.body for qs in queryset])
 
-                headers = {
-                    "Authorization": f"Bearer {token}",
-                    "Content-Type": "application/json"
-                }
+        url = "https://api.orange.com/smsmessaging/v1/outbound/tel%3A%2B2250504522224/requests"
 
-                payload = { 
-                    "outboundSMSMessageRequest": {
-                    "address": f"tel:+225{p}",
-                        "senderAddress":"tel:+2250504522224",
-                        "senderName": "MUPEPPBO",
-                        "outboundSMSTextMessage": {
-                            "message": f"{body}"
-                        }
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json"
+        }
+
+        for nb in m_list:
+            payload = { 
+            "outboundSMSMessageRequest": {
+		            "address": f"tel:+225{nb}",
+		            "senderAddress":"tel:+2250504522224",
+                    "senderName": "MUPEPPBO",
+		            "outboundSMSTextMessage": {
+                        "message": f"{body}"
                     }
-                }
-                response = requests.post(url, data=payload, headers=headers)
+	            }
+            }
+            response = requests.post(url, json=payload, headers=headers)
+
+            json_response = response.json()
+            print(json_response)
