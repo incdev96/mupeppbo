3f90b667276395215f5662eb7344eafcb54da7b3
goodcodelove <coulibalyissa956@gmail.com>
2023-01-19 16:52:04 +0000

    hot fix for send sms function

--- message/admin.py
+++ message/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Mutualist, Sms, Balance
+from .models import Mutualist, Sms
 import requests
 import environ
 from django.conf import settings
-from .tasks import generate_token, get_sms
+from .tasks import generate_token
 
 env = environ.Env()
 environ.Env.read_env(env_file=str(settings.BASE_DIR/"mupeppbo_project"/".env"))
@@ -11,6 +11,7 @@ environ.Env.read_env(env_file=str(settings.BASE_DIR/"mupeppbo_project"/".env"))
 @admin.register(Mutualist)
 class MutualistAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'phone_number']
+    search_fields = ['full_name', 'phone_number']
 
 
 @admin.register(Sms)
@@ -21,37 +22,56 @@ class SmsAdmin(admin.ModelAdmin):
 
     @admin.action(description="Envoyer sms")
     def send_sms(self, request, queryset):
-        token = generate_token()
-        m = []
-        for q in queryset:
-            m += q.mutualist.all()
-        m_list = [m.phone_number for m in m]
-        body = ' '.join([qs.body for qs in queryset])
+        # token = generate_token()
+        # m = []
+        # for q in queryset:
+        #     m += q.mutualist.all()
+        # m_list = [m.phone_number for m in m]
+        # body = ' '.join([qs.body for qs in queryset])
 
-        url = "https://api.orange.com/smsmessaging/v1/outbound/tel%3A%2B2250504522224/requests"
+        # url = "https://api.orange.com/smsmessaging/v1/outbound/tel%3A%2B2250504522224/requests"
 
-        headers = {
-            "Authorization": f"Bearer {token}",
-            "Content-Type": "application/json"
-        }
+        # headers = {
+        #     "Authorization": f"Bearer {token}",
+        #     "Content-Type": "application/json"
+        # }
 
-        for nb in m_list:
-            payload = { 
-            "outboundSMSMessageRequest": {
-		            "address": f"tel:+225{nb}",
-		            "senderAddress":"tel:+2250504522224",
-                    "senderName": "MUPEPPBO",
-		            "outboundSMSTextMessage": {
-                        "message": f"{body}"
+        # for nb in m_list:
+        #     payload = { 
+        #     "outboundSMSMessageRequest": {
+		#             "address": f"tel:+225{nb}",
+		#             "senderAddress":"tel:+2250504522224",
+        #             "senderName": "MUPEPPBO",
+		#             "outboundSMSTextMessage": {
+        #                 "message": f"{body}"
+        #             }
+	    #         }
+        #     }
+        #     response = requests.post(url, json=payload, headers=headers)
+
+        #     json_response = response.json()
+        #     print(json_response)
+        token = generate_token
+        for qs in queryset:
+            body = qs.body
+            phone_number = [m.phone_number for m in qs.mutualist.all()]
+        
+            for p in phone_number:
+                url = "https://api.orange.com/smsmessaging/v1/outbound/tel%3A%2B2250504522224/requests"
+
+                headers = {
+                    "Authorization": f"Bearer {token}",
+                    "Content-Type": "application/json"
+                }
+
+                payload = { 
+                    "outboundSMSMessageRequest": {
+                    "address": f"tel:+225{p}",
+                        "senderAddress":"tel:+2250504522224",
+                        "senderName": "MUPEPPBO",
+                        "outboundSMSTextMessage": {
+                            "message": f"{body}"
+                        }
                     }
-	            }
-            }
-            response = requests.post(url, json=payload, headers=headers)
-
-            json_response = response.json()
-            print(json_response)
-
-
-@admin.register(Balance)
-class BalanceAdmin(admin.ModelAdmin):
-    list_display = ['available_units', 'expire_date']
+                }
+                response = requests.post(url, json=payload, headers=headers)
--- message/models.py
+++ message/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from .tasks import generate_token, get_sms
 import requests
 # Create your models here.
 
@@ -9,7 +8,6 @@ class Mutualist(models.Model):
 
     class Meta:
         verbose_name = "Mutualiste"
-        verbose_name_plural = "Mutualistes"
 
     def __str__(self):
         return self.full_name
@@ -18,29 +16,9 @@ class Sms(models.Model):
     mutualist = models.ManyToManyField(Mutualist)
     body = models.TextField('corps du message')
 
-
     class Meta:
         
-        verbose_name = 'Sms à envoyer'
-        verbose_name_plural = 'Sms à envoyer'
+        verbose_name = 'Message'
 
-
-class Balance(models.Model):
-    
-    available_units = models.IntegerField("Sms restants ")
-    expire_date = models.DateTimeField("Expire le ")
-
-    class Meta:
-
-        verbose_name = "Balance"
-        verbose_name_plural = "Balance"
-
-
-    url = "https://api.orange.com/sms/admin/v1/contracts"
-    token = generate_token()
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
-    sms = requests.get(url, headers=headers).json()
-    available_units = sms[0]["availableUnits"]
-    expire_date = sms[0]["expirationDate"]
+    def __str__(self):
+        return f"message {self.id}"
